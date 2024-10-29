891bbcd6b5da4589625625bc88f1b8af23827f85
Shanwaz Ansari <shanwaz8960@gmail.com>
2024-10-29 16:01:13 +0530

    Done

--- ClassLibrary1/Entities/SaveAddress.cs
+++ ClassLibrary1/Entities/SaveAddress.cs
@@ -8,12 +8,14 @@ namespace Domain.Entities
 {
     public class SavedAddress
     {
-        public int AddressId { get; set; }
+        public int Id { get; set; }
         public int UserId { get; set; }
-        public string FullName { get; set; }        
-        public string PhoneNumber { get; set; }     
+        public string Type { get; set; }    
+        public string HouseNo { get; set; }        
+        public string? Floor { get; set; }    
         public string AddressLine1 { get; set; }    
-        public string AddressLine2 { get; set; }    
+        public string LandMark { get; set; }
+        public string PhoneNumber { get; set; }
         public string City { get; set; }            
         public string State { get; set; }           
         public string PostalCode { get; set; }  
